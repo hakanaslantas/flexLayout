40d3392343bb1bbae9a1b260d47df1ab09b74c66
mr-beaver <beavercoding@gmail.com>
2016-05-02 19:58:12 -0700

    added lib require-js and loadCSS-js; modified router trigger mechanism;

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,30 +1,23 @@
 $(document).ready(function(){
 
-	var TestView = Backbone.View.extend({
-		el: $('#test'),
-
-		render: function(){
-			var that = this;
-			$.get('templates/basic.html', function(data){
-				that.$el.append(data);
-			});
-			return this;
-		}
-	});
-
 	var BasicView = Backbone.View.extend({
-		el: '.tab-content #basic',
+		el: '.demo-content',
 		render: function(){
 			var that = this;
 			$.get('templates/basic.html', function(data){
 				that.$el.html('').append(data);
+			});
+			//load prism async to highlight code tags
+			loadCSS('./css/prism.css');
+			require(['./libs/prism.js'], function(prism) {
+			    Prism.highlightAll();
 			});
 			return this;
 		}
 	});
 
 	var AdvanceView = Backbone.View.extend({
-		el: '.tab-content #advance',
+		el: '.demo-content',
 		render: function(){
 			var that = this;
 			$.get('templates/advance.html', function(data){
@@ -35,7 +28,7 @@ $(document).ready(function(){
 	});
 
 	var MoreView = Backbone.View.extend({
-		el: '.tab-content #more',
+		el: '.demo-content',
 		render: function(){
 			var that = this;
 			$.get('templates/more.html', function(data){
@@ -44,17 +37,47 @@ $(document).ready(function(){
 			return this;
 		}
 	});
+
+	var AppView = Backbone.View.extend({
+		el: '#app',
+		events: {
+			'click .demo-tab': 'contentSwitch'
+		},
+		initialize: function(){
+			this.render();
+		},
+		render: function(){
+			//show basic tab when loaded
+			this.$el.find('.demo-basic').click();
+		},
+		contentSwitch: function(e){
+			var $el = $(e.target);
+			this.$el.find('.demo-tab').removeClass('active');
+			$el.addClass('active');
+			//change url to load corresponding views
+			switch($el.data().hash){
+				case '#basic':
+					window.location.hash = $el.data().hash;
+				break;
+				case '#advance':
+					window.location.hash = $el.data().hash;
+				break;
+				case '#more':
+					window.location.hash = $el.data().hash;
+				break;
+				default:
+				break;
+			}
+		}
+
+	});
 	
 	var AppRouter = Backbone.Router.extend({
 		routes: {
-		//	'': 'tabs',
 			'basic': 'basic',
 			'advance': 'advance',
 			'more': 'more'
 		},
-		/*tabs: function(op1, op2){
-			console.log(op1, op2);
-		},*/
 		basic: function(){
 			new BasicView().render();
 		},
@@ -66,22 +89,12 @@ $(document).ready(function(){
 		}
 	});
 
+	//backbone router
 	new AppRouter();
 	Backbone.history.start();
 
+	//kick start
+	var App = new AppView();
 
-	  var hash = window.location.hash;
-	  hash && $('ul.nav a[href="' + hash + '"]').tab('show');
-	 
-	 $('.nav-tabs a').click(function (e) {
-	    //$(this).tab('show');
-	    //var scrollmem = $('body').scrollTop();
-	    window.location.hash = this.hash;
-	    //$('html,body').scrollTop(scrollmem);
-	  });
-
-
-	
-	
 	
 });
