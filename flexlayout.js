443680f3003892a2cf30ad28732cd7ee7008d106
mr-beaver <beavercoding@gmail.com>
2016-05-04 13:04:03 -0700

    corrected error in bower.json; re-release 0.2.0

--- flexlayout.js
+++ flexlayout.js
@@ -1,5 +1,5 @@
 /**
- * flexLayout v0.2.0, http://TBD
+ * flexLayout v0.2.2, http://TBD
  * ===================================
  * Highly customizable easy to use, light weight, layout/split jQuery plugin
  * 
